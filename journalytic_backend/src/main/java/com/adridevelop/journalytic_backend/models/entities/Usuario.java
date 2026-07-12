b97a8def35d74be381ac54237b18a9ca1997b219
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2026-07-12 18:14:01 +0200

    Creado la entidad Usuario con los datos principales (aún en estudio cuales más puedo agregar)

---
+++ journalytic_backend/src/main/java/com/adridevelop/journalytic_backend/models/entities/Usuario.java
@@ -0,0 +1,36 @@
+package com.adridevelop.journalytic_backend.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    @Column
+    private String name;
+    @Column
+    private String apellidos;
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String password;
+}
